d1ef234e4332102f29a74565c520ee7e50713b85
HEEJIN JEON <wjsgmlwls97@gmail.com>
2021-09-12 19:40:32 +0900

    update를 postmapping에서 putmapping으로 변경

--- src/main/java/com/example/demo/test/controller/MemberController.java
+++ src/main/java/com/example/demo/test/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,7 +49,7 @@ public class MemberController {
 		return rData.toString();
 	}
 	
-	@PostMapping("/updatePassword")
+	@PutMapping("/updatePassword")
 	@ResponseBody
 	public String updatePassword(@RequestBody Map<String, Object> map) throws Exception {
 		JSONObject rData = new JSONObject();
